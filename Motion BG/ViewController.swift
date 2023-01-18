73aa5e4860fce0f73fbd4f9cbe932e53dadac252
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-18 01:41:51 +0100

    Initial commit

---
+++ Motion BG/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Motion BG
+//
+//  Created by Alex Marco Steiner on 18/01/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
