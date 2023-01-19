d4d02fbb7b401aecae530481a5d1de95b69873ab
Alex Steiner <65250433+AlexSteiner30@users.noreply.github.com>
2023-01-19 16:09:01 +0100

    Console Line Verision

---
+++ Main.swift
@@ -0,0 +1,45 @@
+
+//
+//  Background.swift
+//  Motion BG
+//
+//  Created by Alex Marco Steiner on 18/01/23.
+//
+
+import Foundation
+import AppKit
+
+
+var count = 0
+
+func LoadBackground(){
+    print("Welcome to Motion BG, this command line tool was developed by Alex Steiner")
+    print("Path to the folder with the frames: ", terminator: "")
+    
+    if let fileURLStringBase = readLine() {
+        while true{
+            do{
+                if count > 266{
+                    count = 0
+                }
+                
+                let fileURL = URL(fileURLWithPath: fileURLStringBase + "frame" + String(count) + ".gif")
+                try NSWorkspace.shared.setDesktopImageURL(fileURL, for: NSScreen.main!)
+                
+                count+=1
+                
+                usleep(100000)
+            }
+            catch{
+                print("An error occured check the if the numbers of frame is correct, the name of the file is frame + count + .gif, also check your path")
+                break
+            }
+        }
+    }
+    else{
+        LoadBackground()
+    }
+    
+}
+
+LoadBackground()
